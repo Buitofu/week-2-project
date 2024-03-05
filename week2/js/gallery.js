373b7a41138ea1c0988549f2fa85ab20ee38003c
Bui Gia Huy <143254504+Buitofu@users.noreply.github.com>
2024-03-05 12:14:54 +0700

    Add files via upload

---
+++ week2/js/gallery.js
@@ -0,0 +1,19 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic) {
+     /* In this function you should 
+        1) change the url for the background image of the div with the id = "image" 
+        to the source file of the preview image
+        
+        2) Change the text  of the div with the id = "image" 
+        to the alt text of the preview image 
+        */
+     var imageSRC = previewPic.src;
+     document.getElementById('image').style.backgroundImage = 'url(' + imageSRC + ')';
+     document.getElementById('image').innerHTML = previewPic.alt;
+}
+
+function unDo() {
+     document.getElementById('image').style.backgroundImage = 'url()' ;
+     document.getElementById('image').innerHTML = "Hover over an image below to display here." ;
+}
